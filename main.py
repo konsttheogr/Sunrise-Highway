a676d9f7bf6c387dec1be9cd4d31cb349fff59db
KonstTheoGR <60752156+konsttheogr@users.noreply.github.com>
2022-11-06 18:17:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from nextcord import Interaction, SlashOption
 from threading import Thread
 from typing import Optional
 
-TOKEN = "MTAzODgzNjE3Njg0NjU5ODI0NQ.G2JgJS.8G5DPvFKV04cyisakebxyG0ge0o-ywtMJzZ7yk"
-
+TOKEN = os.environ['TOKEN']
 client = commands.Bot(command_prefix='-', intents=nextcord.Intents.all())
 
 
@@ -18,33 +17,66 @@ async def on_ready():
                                  activity=activity)
 	print('Ready')
 	
-def check(interaction, cmd):
-    user = interaction.user
-    guild = interaction.guild
-    #if user.id == owner_id:
-        #return True
+	
+def loadCmd(cmd):
+	with open(f"cmds/{cmd}.json") as f:
+		f = json.load(f)
+		"""
+		try:
+			ctn = f['content']
+		except:
+			ctn = None
+		try:
+			title = f['embed']['title']
+		except:
+			title = None
+			
+		try:
+			description = f['embed']['description']
+		except:
+			description = None
+		emb = nextcord.Embed(title=title, description=description)
+		"""
+		
+		emb = nextcord.Embed().from_dict(f['embed'])
+		try:
+			ctn = f['content']
+		except:
+			ctn = None
+		return [ctn, emb]
 
-    if data['maintenance']==True:
-        return "Bot is under maintenance. Try again later."
-    
-    if cmd in data['disabled_cmds']:
-        return "This command is currenty disabled. Try again later."
-    
-    if str(user.id) in data['bled_users']:
-        if int(datetime.timestamp(datetime.now())) < data[bled_users][str(user.id)]:
-            return f"You are blacklisted until <t:{data[bled_users][str(user.id)]}:F>. You can't run (almost) any cmds until then."
+def is_owner():
+    def predicate(interaction: Interaction):
+        return interaction.user.id in [753222096141680651, 867700344250040350] 
         
-    if str(guild.id) in data['bled_servers']:
-        if int(datetime.timestamp(datetime.now())) < data[bled_servers][str(servers.id)]:
-            return f"This server is blacklisted until <t:{data[bled_users][str(user.id)]}:F>. You can't run (almost) any cmds here until then."
-    
+    return application_checks.check(predicate)
+
+@application_checks.check_any(applications_checks.has_role(1017787962001457262), is_owner())
 @client.slash_command(description="Event manager")
 async def event(interaction: Interaction):
-    if check(interaction, "invite"):
-        await interaction.response.send_message(
-            f'Hey, you can invite me to your server by clicking this {INVITE_LINK}'
-        )
-    else:
-        await interaction.response.send_message(check(interaction,
-                                                      'invite'),
-                                                ephemeral=True)
+	cmd = loadCmd('event')
+    	await interaction.response.send_message(content=cmd[0], embed=cmd[1])
+    
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+client.run(TOKEN)
